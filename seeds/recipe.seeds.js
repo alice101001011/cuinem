b5d4067456422da19e5a02eae511c03f35e35669
alice101001011 <alice101001011@gmail.com>
2022-06-11 08:55:24 +0200

    logo added

--- seeds/recipe.seeds.js
+++ seeds/recipe.seeds.js
@@ -1,27 +1,15 @@
-require('../db');
-const recipes = require('../data/recipes');
-const Recipe = require('../models/Recipe.model');
+// require('../db');
+// const recipes = require('../data/recipes');
+// const Recipe = require('../models/Recipe.model');
 
-const createRecipes = async () => {
-  try {
-    await Recipe.create(recipes);
-    console.log(`${recipes.length} recipes created`)
-  } catch (error) {
-    console.error(error);
-  }
-}
-
-createRecipes();
-
-// const recipesJson = require('../recipes.json')
-// const seedRecipes = async () => {
-// try {
-//     await Recipe.deleteMany();
-//     const createRecipes = await Recipe.create(jsonData);
-//     console.log(`${createRecipes.length} recipes created`);
-//     await mongoose.connection.close();
-// }catch(error){
-//     console.log(error);
+// const createRecipes = async () => {
+//   try {
+//     await Recipe.create(recipes);
+//     console.log(`${recipes.length} recipes created`)
+//   } catch (error) {
+//     console.error(error);
+//   }
 // }
-// }
-// seedRecipes();
+
+// createRecipes();
+
